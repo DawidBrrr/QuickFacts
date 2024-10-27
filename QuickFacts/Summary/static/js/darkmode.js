963edcb9a7aafd76a630efe8783f483050382622
Dawid Brach <dbrach6@gmail.com>
2024-10-27 20:56:31 +0100

    Dark Mode button now with svg image

--- QuickFacts/Summary/static/js/darkmode.js
+++ QuickFacts/Summary/static/js/darkmode.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById('dark-mode-toggle');
     const body = document.body;
+    const icon = document.getElementById('mode-icon');
+    
+    // Define paths for icons
+    const sunIconPath = "/static/images/sun-icon.svg";
+    const moonIconPath = "/static/images/moon-icon.svg";
+
 
     // Check for saved preference in localStorage
     const darkMode = localStorage.getItem('dark-mode');
     if (darkMode === 'enabled') {
         body.classList.add('dark-mode');
+        icon.src = moonIconPath; // set icon to moon when dark mode is enabled
+    }
+    else {
+        icon.src = sunIconPath; // set icon to sun when light mode is enabled
     }
 
     // Toggle dark mode on button click
@@ -15,8 +25,10 @@ document.addEventListener("DOMContentLoaded", function () {
         // Save preference to localStorage
         if (body.classList.contains('dark-mode')) {
             localStorage.setItem('dark-mode', 'enabled');
+            icon.src = moonIconPath;
         } else {
             localStorage.removeItem('dark-mode');
+            icon.src = sunIconPath;
         }
     });
 });
--- QuickFacts/Summary/static/js/scripts.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // Example: Display an alert when the form is submitted
-    $('form').on('submit', function() {
-        alert('Wprowadzono link!');
-    });
-});
