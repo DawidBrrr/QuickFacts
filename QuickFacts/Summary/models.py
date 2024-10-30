0c6dbdadae2362d5a505aa95743e449aa3796f44
Dawid Brach <dbrach6@gmail.com>
2024-10-30 22:32:40 +0100

    link and summary Data Model

--- QuickFacts/Summary/models.py
+++ QuickFacts/Summary/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ArticleSummary(models.Model):
+    link = models.URLField(unique=True)
+    summary = models.TextField()
+
+    def __str__(self):
+        return self.link
+    
+
+    #TO DO implement check if exists in views.py
