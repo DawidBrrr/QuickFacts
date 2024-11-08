4269e31b59e3f3699d466ebdc8437eeb2254c571
Dawid Brach <dbrach6@gmail.com>
2024-11-08 17:35:21 +0100

    Article Title Display

--- QuickFacts/Summary/models.py
+++ QuickFacts/Summary/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class ArticleSummary(models.Model):
     link = models.URLField(unique=True)
+    title = models.CharField(max_length=255)
     summary = models.TextField()
 
     def __str__(self):
--- QuickFacts/Summary/views.py
+++ QuickFacts/Summary/views.py
@@ -10,6 +10,7 @@ from transformers import MBartForConditionalGeneration, MBart50Tokenizer, AutoMo
 def home(request):
     submitted_link = None
     summary = None
+    article_title = None
 
     if request.method == 'POST':
         form = LinkForm(request.POST)
@@ -21,6 +22,7 @@ def home(request):
 
             if article_summary:  # Entry already exists
                 summary = article_summary.summary
+                article_title = article_summary.title
             else:
                 # Proceed with scraping and summarization if new entry
                 try:
@@ -29,18 +31,10 @@ def home(request):
 
                     # Parse the content
                     soup = BeautifulSoup(response.content, 'html.parser')
-                    article = soup.find('article')
-                    
-                    if article:
-                        paragraphs = article.find_all('p')
-                        article_content = "\n\n".join([p.get_text() for p in paragraphs])
-                    else:
-                        content_div = soup.find('div', class_='article-content')
-                        if content_div:
-                            paragraphs = content_div.find_all('p')
-                            article_content = "\n\n".join([p.get_text() for p in paragraphs])
-                        else:
-                            article_content = "No article content found at the provided URL."
+
+                    article_title = extract_article_title(soup)
+
+                    article_content = extract_article_content(soup)                   
 
                     # Summarize if content is available
                     if not article_content.strip():
@@ -49,7 +43,7 @@ def home(request):
                         summary = summarize_content(article_content)
 
                         # Save the link and summary in the database
-                        article_summary = ArticleSummary(link=submitted_link, summary=summary)
+                        article_summary = ArticleSummary(link=submitted_link,title=article_title, summary=summary)
                         article_summary.save()
 
                 except requests.exceptions.RequestException as e:
@@ -63,6 +57,7 @@ def home(request):
             return render(request, 'Summary/home.html', {
                 'form': form,
                 'submitted_link': submitted_link,
+                'article_title': article_title,
                 'summary': summary,
             })
 
@@ -72,6 +67,33 @@ def home(request):
     return render(request, 'Summary/home.html', {
         'form': form,
     })
+
+def extract_article_title(soup):
+    #Check for <title>
+    if soup.title:
+        return soup.title.get_text().strip()
+    
+    #Check for <h1>
+    h1 = soup.find('h1')
+    if h1:
+        return h1.get_text().strip()
+    
+
+    return "Artykuł"
+
+def extract_article_content(soup):
+    article = soup.find('article')
+    #Try to find <article> tag 
+    if article:
+        paragraphs = article.find_all('p')
+        return "\n\n".join([p.get_text() for p in paragraphs])
+    #If not found in <article> try conent inside <div> with specyfic class
+    content_div = soup.find('div', class_='article-content')
+    if content_div:
+        paragraphs = content_div.find_all('p')
+        return "\n\n".join([p.get_text() for p in paragraphs])
+    
+    return "No article content found at the provided URL."
 
 
 
@@ -83,13 +105,13 @@ def summarize_chunk(chunk, model, tokenizer, device):
 
     summary_ids = model.generate(
         inputs['input_ids'],
-        max_length=200,  # Adjust to control summary length for each chunk
-        min_length=50,
+        max_length=120,  # Adjust to control summary length for each chunk
+        min_length=40,
         num_beams=4,
         length_penalty=2.0,
         early_stopping=True,
         forced_bos_token_id=tokenizer.lang_code_to_id["pl_PL"],
-        temperature=1.0,
+        temperature=1,
     )
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
@@ -140,8 +162,8 @@ def summarize_content(article_content):
     # Set the source language for Polish
     mbart_tokenizer.src_lang = "pl_PL"
 
-    # Split the article content into chunks (let's use 4000 tokens per chunk for BigBird)
-    chunks = split_into_chunks(article_content, max_length=4096,tokenizer=mbart_tokenizer)
+    
+    chunks = split_into_chunks(article_content, max_length=1024,tokenizer=mbart_tokenizer)
 
     # Summarize each chunk separately
     summaries = []
