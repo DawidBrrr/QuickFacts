90ccaf880b94bd329cab8276a58600434d9a3cfe
Dawid Brach <dbrach6@gmail.com>
2024-11-10 21:20:32 +0100

    Project Structure Rework

--- QuickFacts/QuickFacts/settings.py
+++ QuickFacts/QuickFacts/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Summary',
+    'ArticleSearch',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'QuickFacts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'QuickFacts','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Summary/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'QuickFacts','static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
